02df023624c8e938f2afb8d67320e4f644400f50
sp9wpn <sp9wpn@gmail.com>
2021-01-14 01:20:54 +0100

    nicelist: added sorting by distance

--- nicelist/README.md
+++ nicelist/README.md
@@ -2,6 +2,8 @@
 
 This short tool converts /tmp/sonde.csv to easy-to-read list.
 Great for live monitoring receiver status on the console.
+Use -lat and -lon parameters to sort sondes by distance.
 
 Skrypt konwertuje zawartość /tmp/sonde.csv na czytelną listę.
 Polecam do monitorowania pracy odbiornika na konsoli.
+Użyj parametrów -lat oraz -lon aby sortować sondy wg odległości.
--- nicelist/nicelist.py
+++ nicelist/nicelist.py
@@ -4,9 +4,43 @@
 import time
 import csv
 import os
+import argparse
+from math import sin, cos, sqrt, atan2, radians
 
 
-def ps(row):
+argparser = argparse.ArgumentParser(description='Readable presentation of spdxl live data')
+
+argparser.add_argument('-lat', metavar='<lat>', type=float, help='Station latitude', default=0)
+argparser.add_argument('-lon', metavar='<lon>', type=float, help='Station longitude', default=0)
+
+args=argparser.parse_args()
+
+qth=(args.lat,args.lon)
+
+
+def calc_distance(p1,p2):
+  r = 6373.0
+
+  lat1 = radians(float(p1[0]))
+  lon1 = radians(float(p1[1]))
+  lat2 = radians(float(p2[0]))
+  lon2 = radians(float(p2[1]))
+
+  dlon = lon2 - lon1
+  dlat = lat2 - lat1
+
+  a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+  c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+  distance = r * c
+  return int(distance)
+
+
+def sortFunc(e):
+  return(e[7])
+
+
+def parserow(row):
   try:
     t = int(row[8])
   except:
@@ -30,8 +64,9 @@ def ps(row):
   else:
     vs = "%3.1f" % abs(float(row[5])) + vss
 
-  print (u"%5s%9s %7.4f %7.4f %5dm %s %7.3f" %
-	(ttime, row[0], float(row[1]), float(row[2]), int(row[3]), vs, float(row[7])))
+  dist = calc_distance(qth,(row[1],row[2]))
+
+  return (ttime, row[0], float(row[1]), float(row[2]), int(row[3]), vs, float(row[7]), dist)
 
 
 _=os.system("clear")
@@ -39,8 +74,18 @@ while True:
   try:
     csvreader = csv.reader(open('/tmp/sonde.csv', 'r'), delimiter=';', quoting=csv.QUOTE_NONE)
     _=os.system("clear")
+    list=[]
     for row in sorted(csvreader, key=lambda row: row[0]):
-      ps(row)
+      parsed=parserow(row)
+      if parsed != None:
+        list.append(parsed)
+
+    list.sort(key=sortFunc)
+
+    for l in list:
+      print (u"%5s%9s %7.4f %7.4f %5dm %s %7.3f" %
+	(l[0], l[1], float(l[2]), float(l[3]), int(l[4]), l[5], float(l[6])))
+
   except:
     pass
 
